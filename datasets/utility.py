0097f77e4cc366cac475fa38f7b3802247dc6610
qiman <wangqiman0111@gmail.com>
2017-09-23 20:43:05 -0400

    added all_start_count

--- datasets/utility.py
+++ datasets/utility.py
@@ -30,7 +30,9 @@ def num_of_dates_till_end_of_season(game_day):
 # print(num_of_dates_till_end_of_season(df['date_est'][12000]))
 
 ########################################################################
-# returns player_id, name, jersey_sale_rank
+# returns 2 list
+# list1 = team1{ date, player_id, name, jersey_sale_rank}
+# list2 = team2{ date, player_id, name, jersey_sale_rank}
 def jersey_sales_on_teams(team1, team2, game_date, game_df, jersey_df, player_df):
     game_id_df = game_df.loc[(game_df['date_est'] == game_date) & (game_df['Team'] == team1)]
     game_id = game_id_df['Game_id'][game_id_df.index.get_values()[0]]
@@ -51,7 +53,7 @@ def jersey_sales_on_teams(team1, team2, game_date, game_df, jersey_df, player_df
     # print('t', target_year, 'col1', column1, "col2", column2)
 
     t1_jersey_player = 0
-    t1_list =[]
+    t1_list = [game_date]
     for index_t1, row_t1 in team1_players_df.iterrows():
         name1 = row_t1['Person_id']
         actual_name1 = row_t1['Name']
@@ -59,11 +61,11 @@ def jersey_sales_on_teams(team1, team2, game_date, game_df, jersey_df, player_df
             # print(row_j[column1], name1, row_j[column2])
             if row_j[column1] == name1 or row_j[column2] == name1:
                 t1_jersey_player += 1
-                t1_list.append((name1,actual_name1,int(index_j)+1))
+                t1_list.append((name1, actual_name1, int(index_j) + 1))
     # print(t1_jersey_player)
 
     t2_jersey_player = 0
-    t2_list = []
+    t2_list = [game_date]
     for index_t2, row_t2 in team2_players_df.iterrows():
         name2 = row_t2['Person_id']
         actual_name2 = row_t2['Name']
@@ -76,10 +78,45 @@ def jersey_sales_on_teams(team1, team2, game_date, game_df, jersey_df, player_df
     return (t1_list, t2_list)
 
 
+# # test case
+# player_df = pd.read_csv('Player_Data.csv')
+# jersey_df = pd.read_csv('Jersey_Sales_Rankings_Data.csv')
+# game_df = pd.read_csv('Game_Data.csv')
+# t1_jersery_count, t2_jersey_count = jersey_sales_on_teams('SAS', 'CHA', '3/21/2016', game_df, jersey_df, player_df)
+# print(t1_jersery_count,t2_jersey_count)
+
+#######################################################################################################################
+#
+def all_star_players_in_game(team1, team2, game_date, game_df, player_df):
+    game_id_df = game_df.loc[(game_df['date_est'] == game_date) & (game_df['Team'] == team1)]
+    game_id = game_id_df['Game_id'][game_id_df.index.get_values()[0]]
+    team1_players_df = player_df.loc[(player_df['Game_id'] == game_id) & (player_df['Team'] == team1)]
+
+    game_id_df = game_df.loc[(game_df['date_est'] == game_date) & (game_df['Team'] == team2)]
+    game_id = game_id_df['Game_id'][game_id_df.index.get_values()[0]]
+    team2_players_df = player_df.loc[(player_df['Game_id'] == game_id) & (player_df['Team'] == team2)]
+
+    month = game_date.split('/')[0]
+    target_year = int(game_date.split('/')[-1])
+    if int(month) <= 4:
+        target_year = target_year - 1
+    print(target_year)
+    t1_list = [game_id, game_date]
+    for index, row in team1_players_df.iterrows():
+        if row['ASG_Team'] != 'None':
+            t1_list.append((row['Person_id'], row['Name'], row['ASG_Team']))
+
+    t2_list = [game_id, game_date]
+    for index, row in team2_players_df.iterrows():
+        if row['ASG_Team'] != 'None':
+            t2_list.append((row['Person_id'], row['Name'], row['ASG_Team']))
+
+    return (t1_list, t2_list)
+
+
 # test case
 player_df = pd.read_csv('Player_Data.csv')
 jersey_df = pd.read_csv('Jersey_Sales_Rankings_Data.csv')
 game_df = pd.read_csv('Game_Data.csv')
-# num_mvp_on_teams('BOS', 'MIA', '10/26/2010', game_df, jersey_df, player_df)
-t1_jersery_count, t2_jersey_count = jersey_sales_on_teams('SAS', 'CHA', '3/21/2016', game_df, jersey_df, player_df)
-print(t1_jersery_count,t2_jersey_count)
+t1_all_star_count, t2_all_star_count = all_star_players_in_game('SAS', 'CHA', '3/21/2016', game_df, player_df)
+print(t1_all_star_count, t2_all_star_count)
